4d53cf47a7a8352444f702758ba955aa0c022376
Ankur123unsungDevloper <ankurdas1804@gmail.com>
2025-04-22 22:10:25 +0530

    new file:   app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx 	new file:   app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx 	new file:   app/(auth)/layout.tsx 	modified:   app/layout.tsx 	new file:   middleware.ts 	modified:   package-lock.json 	modified:   package.json

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import type { Metadata } from "next";
 import "./globals.css";
+import { ClerkProvider } from '@clerk/nextjs'
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -12,10 +14,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
