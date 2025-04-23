6be0771993217bf144191cd57ee51613e52eca3c
Ankur123unsungDevloper <ankurdas1804@gmail.com>
2025-04-24 01:23:33 +0530

    modified:   app/(root)/page.tsx 	new file:   components/ui/form.tsx 	new file:   components/ui/label.tsx 	modified:   package-lock.json 	modified:   package.json

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,22 @@
 "use client";
 
-import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
+
 
 const SetupPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
   return (
     <div>
-      <Modal title="Title" description="Title description" isOpen onClose={() => { }}>
-        Children
-      </Modal>
+      
     </div>
   );
 }
